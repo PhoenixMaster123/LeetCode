77ee404aff4a6de6a36abf6505a19be4dbc5cdaa
Kristian Popov <kristianpopov03@yahoo.com>
2023-07-06 01:27:22 +0300

    Build Array from Permutation

---
+++ Easy Problems/2-Build Array from Permutation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _2_Build_Array_from_Permutation
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            int[] ans = BuildArray(nums);
+            Console.WriteLine(string.Join(", ", ans));
+        }
+
+        private static int[] BuildArray(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[nums[i]];
+            }
+            return ans;
+        }
+    }
+}
