278c213f7e6f1f1c7024c036110edaba4de20672
Kristian Popov <kristianpopov03@yahoo.com>
2023-07-07 11:57:58 +0300

    Add Two Integers

---
+++ Easy Problems/Maths Problems/2235 - Add Two Integers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2235___Add_Two_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Sum(num1,num2));
+        }
+
+        private static int Sum(int num1, int num2)
+        {
+          return num1 + num2;
+        }
+    }
+}
