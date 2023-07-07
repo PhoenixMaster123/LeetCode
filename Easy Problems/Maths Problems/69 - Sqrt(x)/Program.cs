56efe6393982dfa9381f51bc730c39a2bcc3276e
Kristian Popov <kristianpopov03@yahoo.com>
2023-07-07 12:22:23 +0300

    Sqrt(x)

---
+++ Easy Problems/Maths Problems/69 - Sqrt(x)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _69___Sqrt_x_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(MySqrt(x));
+        }
+
+        private static int MySqrt(int x)
+        {
+           double square = Math.Sqrt(x);
+           double result = Math.Floor(square);
+           int finall = (int)result;
+           return finall;
+
+        }
+    }
+}
