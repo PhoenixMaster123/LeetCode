d98d8a72be5e7782f49a9a665aa05d7abc52d50d
Kristian Popov <kristianpopov03@yahoo.com>
2023-07-07 12:07:03 +0300

    Valid Perfect Square

---
+++ Easy Problems/Maths Problems/367 - Valid Perfect Square/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Valid_Perfect_Square
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsPerfectSquare(num));
+        }
+
+        private static bool IsPerfectSquare(int num)
+        {
+            bool result = false;
+            if (num < 0)
+            {
+                return result;
+            }
+            if (num == 0 || num == 1)
+            {
+                result = true;
+                return result;
+            }
+            for (int i = 0; i <= num / 2; i++)
+            {
+                if (i * i == num)
+                {
+                    result = true;
+                    return result;
+                }
+            }
+            return result;
+        }
+
+
+    }
+}
