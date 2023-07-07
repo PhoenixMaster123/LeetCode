e857b172154b1f7b6c471f780181a29e0bf95d41
Kristian Popov <kristianpopov03@yahoo.com>
2023-07-07 17:28:54 +0300

    Concatenation of Array

---
+++ Easy Problems/Array Problems/1929 - Concatenation of Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1929___Concatenation_of_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join(",",GetConcatenation(nums)));
+        }
+
+        private static int[] GetConcatenation(int[] nums)
+        {
+            List<int> ints = new List<int>(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ints.Add(nums[i]);
+            }
+            return ints.ToArray();
+        }
+    }
+}
