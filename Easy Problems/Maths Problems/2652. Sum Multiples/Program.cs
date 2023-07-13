e83aaaa1266407e5a87ea5cd0ad87c9356c88528
Kristian Popov <kristianpopov03@yahoo.com>
2023-07-13 21:38:39 +0300

    Sum Multiples

---
+++ Easy Problems/Maths Problems/2652. Sum Multiples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2652._Sum_Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumOfMultiples(n));
+        }
+
+        private static int SumOfMultiples(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
