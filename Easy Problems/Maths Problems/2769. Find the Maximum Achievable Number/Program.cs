c4b39111b4749171552a9b8ed466dc7951d42148
Kristian Popov <kristianpopov03@yahoo.com>
2023-07-13 21:39:23 +0300

    Find the Maximum Achievable Number

---
+++ Easy Problems/Maths Problems/2769. Find the Maximum Achievable Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2769._Find_the_Maximum_Achievable_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine(TheMaximumAchievableX(num,t));
+        }
+
+        private static int TheMaximumAchievableX(int num, int t)
+        {
+            int x = num + t + t;
+            return x;
+        }
+    }
+}
